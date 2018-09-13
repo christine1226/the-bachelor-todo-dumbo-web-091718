e48a7a0a302a6650469c4119bdd7be1765d47dbe
christine gomez <christine.gomez1226@gmail.com>
2018-09-13 22:56:36 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -40,5 +40,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  contestant_age = data[season].collect do |contestant|
+    contestant["age"].to_i
 end
+  
