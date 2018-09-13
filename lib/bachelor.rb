2ad466ffe744628a8900e6b8c7a7f8d6b6e57e80
christine gomez <christine.gomez1226@gmail.com>
2018-09-13 22:49:29 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,6 +4,15 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
+  person = nil
+  data.each do |hash_season, contestant_array|
+    contestant_array.find do |contestant|
+      if contestant["occupation" == occupation]
+        person = contestant["name"]
+      end
+    end
+  end
+  person
 end
 
 def count_contestants_by_hometown(data, hometown)
