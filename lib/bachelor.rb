d8f31e89600af9e26c5de7a186d839bade7813a5
christine gomez <christine.gomez1226@gmail.com>
2018-09-13 23:12:28 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -34,7 +34,7 @@ end
 def get_occupation(data, hometown)
   person = 0
   data.each do |hash_season, contestants_array|
-    contestants_array.each do |contestant|
+    contestants_array.collect do |contestant|
       if contestant["hometown"] == hometown
         person = contestant["occupation"]
       end
